0ec95080de21f153cc1e1218b00124863faba115
Piwero <pinero@Joses-MacBook-Air.local>
2021-02-26 12:47:05 +0000

    add str to board model

--- forumproject/coreapp/models.py
+++ forumproject/coreapp/models.py
@@ -6,6 +6,9 @@ class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
@@ -21,3 +24,4 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
+
