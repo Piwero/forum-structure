3d805cf800126dc30c7b9e6c309c7fe517f4b069
Piwero <pinero@Joses-MacBook-Air.local>
2021-03-02 17:04:51 +0000

    add topics and tests

--- forumproject/urls.py
+++ forumproject/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from .boards import views
 
+
 urlpatterns = [
+    path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('admin/', admin.site.urls),
     path("", views.home, name='home'),
+
 ]
